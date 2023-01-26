4a2301a4a3751dd7eef0f887c802e0d1e0e47f09
Cymbospondylus <2810560593@qq.com>
2023-01-26 19:41:04 +0800

    init

---
+++ src/main/java/site/byzl/cymcomments/CymCommentsApplication.java
@@ -0,0 +1,13 @@
+package site.byzl.cymcomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CymCommentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CymCommentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/byzl/cymcomments/CymCommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.byzl.cymcomments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CymCommentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
